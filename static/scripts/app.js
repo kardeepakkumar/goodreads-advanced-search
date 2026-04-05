6523fd2a0e0f0336894560bd66fde2ced6d74231
Kar Deepak Kumar <deepakkumaar97@gmail.com>
2026-04-05 17:44:57 +0530

    clean slate

--- static/scripts/app.js
+++
@@ -1,9 +0,0 @@
-import { setupTabs } from './tabs.js';
-import { setupPagination } from './pagination.js';
-import { setupScraping } from './scrape.js';
-
-document.addEventListener("DOMContentLoaded", () => {
-    setupTabs();
-    setupPagination(window.totalPages, window.currentPage);
-    setupScraping();
-});
